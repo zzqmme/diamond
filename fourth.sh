17451ca4893e41db95ae6e1bb0b1be833e719e9d
zzqmme <43514109+zzqmme@users.noreply.github.com>
2022-11-09 16:07:09 +0300

    Update fourth.sh

--- fourth.sh
+++ fourth.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+if [ "$#" -ne "1" ]; then
+	echo "ERROR: Invalid number of arguments"
+	exit 1
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+  echo "ERROR: Not a number"
+  exit 1
+fi
+
 n=$1
 first=1
-for((i=0;i<n;i++))
-do
-	for((j=0;j<((n-first));j++))
-	do
+#первая половина ромба
+for((i=0;i<n;i++)); do
+	for((j=0;j<((n-first));j++)); do
 		echo -n " "
 	done
-	for ((k = 0;k < $first; k++))
-	do
+	for ((k = 0;k < $first; k++)); do
 		echo -n "* "
 	done
 	echo ""
@@ -17,14 +25,12 @@ do
 done
 
 second=n-1
-for ((i = 0; i<=((n - 1)); i++))
-do
-	for ((j = 0; j < ((n-second));j++))
-	do
+#Вторая половина ромба
+for ((i = 0; i<=((n - 1)); i++)); do
+	for ((j = 0; j < ((n-second));j++)); do
 		echo -n " "
 	done
-	for ((k = 0; k < second; k++))
-	do
+	for ((k = 0; k < second; k++)); do
 		echo -n "* "
 	done
 	echo ""
